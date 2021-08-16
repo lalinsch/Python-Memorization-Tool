30f02821215c1deb795a46c6cb7ed0e4fde9a350
lalinsch <laloschjetnan@gmail.com>
2021-08-16 17:00:02 +0100

    Finished with Stage 4

--- tests.py
+++ tests.py
@@ -1,7 +1,9 @@
-from hstest.stage_test import StageTest
+import os
+
 from hstest.check_result import CheckResult
-from hstest.test_case import TestCase, SimpleTestCase
 from hstest.exceptions import WrongAnswerException
+from hstest.stage_test import StageTest
+from hstest.test_case import TestCase
 
 MAIN_MENU = """
 1. Add flashcards
@@ -10,11 +12,27 @@ MAIN_MENU = """
 SUB_MENU = """
 1. Add a new flashcard
 2. Exit"""
+UPDATE_MENU = """
+press "d" to delete the flashcard:
+press "e" to edit the flashcard:
+"""
+CHECK_LEARN_MENU = """
+press "y" if your answer is correct:
+press "n" if your answer is wrong:
+"""
 FIRST_QUESTION = "What is the Capital city of Germany?"
 FIRST_ANSWER = "Berlin"
 SECOND_QUESTION = "What is the Capital city of Italy?"
 SECOND_ANSWER = "Rome"
-Q_S = 'Please press "y" to see the answer or press "n" to skip:'
+NEW_SECOND_QUESTION = "Italy?"
+NEW_SECOND_ANSWER = "Rome is the capital of Italy"
+Q_S = """
+press "y" to see the answer:
+press "n" to skip:
+press "u" to update:
+"""
+if os.path.exists('flashcard.db'):
+    os.remove('flashcard.db')
 
 
 class FlashCardTest(StageTest):
@@ -29,12 +47,14 @@ class FlashCardTest(StageTest):
                                 self.test1_input8,
                                 self.test1_input9,
                                 self.test1_input10,
+                                'n',
                                 self.test1_input11,
+                                'n',
                                 self.test1_input12,
-                                ])
-            , TestCase(stdin=[self.test2_input1,
-                              self.test2_input2,
-                              self.test2_input3])
+                                ]),
+                TestCase(stdin=[self.test2_input1,
+                                self.test2_input2,
+                                self.test2_input3])
             , TestCase(stdin=['1',
                               '4',
                               self.test3_input1,
@@ -49,7 +69,64 @@ class FlashCardTest(StageTest):
                               'What is the capital city of Peru?',
                               ' ',
                               self.test5_input5,
-                              self.test5_input6])]
+                              self.test5_input6])
+            , TestCase(stdin=['2',
+                              self.test1_input10,
+                              'n',
+                              self.test1_input11,
+                              'n',
+                              self.test1_input12
+                              ])
+            , TestCase(stdin=['2',
+                              'u',
+                              self.test7_input3,
+                              'u',
+                              'e',
+                              self.test7_input6,
+                              self.test7_input7,
+                              self.test7_input8])
+            , TestCase(stdin=['2',
+                              self.test8_input2,
+                              self.test8_input3])
+            , TestCase(stdin=['2',
+                              'y',
+                              self.test9_input3])
+            , TestCase(stdin=['2',
+                              'n',
+                              'y',
+                              '2',
+                              'y',
+                              'y',
+                              '2',
+                              'n',
+                              'y',
+                              '2',
+                              self.test10_input11])
+            , TestCase(stdin=['1',
+                              '1',
+                              FIRST_QUESTION,
+                              FIRST_ANSWER,
+                              '2',
+                              '2',
+                              'n',
+                              'y',
+                              '2',
+                              'n',
+                              'y',
+                              '2',
+                              'n',
+                              'n',
+                              '2',
+                              'n',
+                              'y',
+                              '2',
+                              'y',
+                              'y',
+                              '2',
+                              'y',
+                              'y',
+                              '2',
+                              self.test10_input11])]
 
     def check_main_menu(self, out):
         main_menu_list = MAIN_MENU.strip().split('\n')
@@ -83,25 +160,44 @@ class FlashCardTest(StageTest):
 
     def check_practice_question(self, out, question):
         out_list = out.strip().split('\n')
-        out_first_line = out_list[0].split(':')
-        if not 'Question' == out_first_line[0]:
-            raise WrongAnswerException(f'{out_first_line[0]} is wrong!\nplease check extra spaces, misspelling or ":"')
-        if not question == out_first_line[1].strip():
-            raise WrongAnswerException('wrong question is printed')
-        if not Q_S == out_list[1]:
-            raise WrongAnswerException(f'{out_list[1]} is wrong\n{Q_S} is correct')
-        return True
+        if len(out_list) < 2:
+            raise WrongAnswerException("output can't be empty")
+        else:
+            out_first_line = out_list[0].split(':')
+            if len(out_first_line) == 0:
+                raise WrongAnswerException("output can't be empty")
+            else:
+                if not 'Question' == out_first_line[0]:
+                    raise WrongAnswerException(
+                        f'The line \"{out_first_line[0]}\" is wrong!\nPlease check extra spaces, misspelling or ":"')
+                if len(out_list) < 4:
+                    raise WrongAnswerException("Incorrect number of lines is found for the menu after the question \n"
+                                               f"\"{out_first_line[0]}\"\n"
+                                               f"Please, format your output according to the example.")
+                question_menu = f'{out_list[1]}\n{out_list[2]}\n{out_list[3]}'
+                if not question == out_first_line[1].strip():
+                    raise WrongAnswerException('wrong question is printed')
+                if not Q_S.strip() == question_menu.strip():
+                    raise WrongAnswerException(f'{question_menu} is wrong\n{Q_S} is correct')
+                return True
 
     def check_practice_answer(self, out, answer):
         out_list = out.strip().split('\n')
-        first_part = out_list.pop(0).strip().split(':')
-        if not 'Answer' == first_part[0]:
-            raise WrongAnswerException('Answer:\n is missing or misspelling!')
-        if not answer == first_part[1].strip():
-            raise WrongAnswerException('The answer is not printed correctly')
-        if Q_S.strip() in out_list:
-            return True
-        raise WrongAnswerException(f'{Q_S}\n should be printed after answer')
+        q_s_list = Q_S.strip().split('\n')
+        if len(out_list) == 0:
+            raise WrongAnswerException("output can't be empty")
+        else:
+            first_part = out_list.pop(0).strip().split(':')
+            if len(first_part) == 0:
+                raise WrongAnswerException("output can't be empty")
+            else:
+                if not 'Answer' == first_part[0]:
+                    raise WrongAnswerException('Answer:\n is missing or misspelling!')
+                if not answer == first_part[1].strip():
+                    raise WrongAnswerException('The answer is not printed correctly')
+                if q_s_list[0] in out_list and q_s_list[1] in out_list and q_s_list[2] in out_list:
+                    return True
+                raise WrongAnswerException(f'{Q_S}\nshould be printed after answer')
 
     def test1_input1(self, out):
         if self.check_main_menu(out):
@@ -122,8 +218,6 @@ class FlashCardTest(StageTest):
     def test1_input5(self, out):
         if self.check_sub_menu(out):
             return '1'
-        # if result == 1 or result == 2:
-        #     raise WrongAnswerException('after entering answer the sub_menu must be printed')
 
     def test1_input6(self, out):
         if self.check_question(out):
@@ -136,14 +230,10 @@ class FlashCardTest(StageTest):
     def test1_input8(self, out):
         if self.check_sub_menu(out):
             return '2'
-        # if result == 1 or result == 2:
-        #     raise WrongAnswerException('after entering answer the sub_menu must be printed')
 
     def test1_input9(self, out):
         if self.check_main_menu(out):
             return '2'
-        # if result == 1 or result == 2:
-        #     raise WrongAnswerException('by press no.2 it should return to main menu')
 
     def test1_input10(self, out):
         if self.check_practice_question(out, FIRST_QUESTION):
@@ -154,11 +244,9 @@ class FlashCardTest(StageTest):
         if len(question) == 0:
             raise WrongAnswerException("The output can't be empty")
         else:
-            question.pop(0)
             question = '\n'.join(question)
             if self.check_practice_question(question.strip(), SECOND_QUESTION):
-                if self.check_practice_answer(out, FIRST_ANSWER):
-                    return 'n'
+                return 'n'
 
     def test1_input12(self, out):
         if self.check_main_menu(out.strip()):
@@ -240,6 +328,113 @@ class FlashCardTest(StageTest):
         output = out.strip().split('\n')
         if not 'Answer:' in output:
             raise WrongAnswerException("the answer can't be empty!")
+        return CheckResult.correct()
+
+    def test7_input3(self, out):
+        out_put_list = out.strip().split('\n')
+        update_menu_list = UPDATE_MENU.strip().split('\n')
+        if len(out_put_list) > 3 or len(out_put_list) < 2:
+            raise WrongAnswerException(f'The update_menu has "2" lines and it should be like this: \n{UPDATE_MENU}')
+        for index, content in enumerate(update_menu_list):
+            if not content == out_put_list[index]:
+                raise WrongAnswerException(f'The line no.{index + 1} should be like this: {content}')
+        return 'd'
+
+    def test7_input6(self, out):
+        output_list = out.strip().split('\n')
+        if len(output_list) == 0:
+            raise WrongAnswerException("The output can't be empty")
+        else:
+            first_line = output_list[0].split(':')
+            if len(first_line) < 2:
+                raise WrongAnswerException("The ':' is missing in output")
+            else:
+                second_line = output_list[1]
+                if not first_line[0].strip() == "current question":
+                    raise WrongAnswerException('current question\nis missing or misspelling')
+                if not first_line[1].strip() == SECOND_QUESTION:
+                    raise WrongAnswerException(' after current question: question should be printed')
+                if not second_line == 'please write a new question:':
+                    raise WrongAnswerException('please write a new question: \nis missing or misspelling')
+
+        return NEW_SECOND_QUESTION
+
+    def test7_input7(self, out):
+        output_list = out.strip().split('\n')
+        if len(output_list) == 0:
+            raise WrongAnswerException("The output can't be empty")
+        else:
+            first_line = output_list[0].split(':')
+            if len(first_line) < 2:
+                raise WrongAnswerException("The ':' is missing in output")
+            else:
+                second_line = output_list[1]
+                if not first_line[0].strip() == "current answer":
+                    raise WrongAnswerException('current answer:\nis missing or misspelling')
+                if not first_line[1].strip() == SECOND_ANSWER:
+                    raise WrongAnswerException(' after current answer: answer should be printed')
+                if not second_line == 'please write a new answer:':
+                    raise WrongAnswerException('please write a new answer: \nis missing or misspelling')
+        return NEW_SECOND_ANSWER
+
+    def test7_input8(self, out):
+        if self.check_main_menu(out):
+            return CheckResult.correct()
+
+    def test8_input2(self, out):
+        out_put_list = out.strip().split('\n')
+        if len(out_put_list) == 0:
+            raise WrongAnswerException("The output can't be empty")
+        else:
+            first_line = out_put_list[0].strip().split(':')
+            if not first_line[1].strip() == NEW_SECOND_QUESTION:
+                raise WrongAnswerException("update function doesn't update question properly")
+
+        return 'y'
+
+    def test8_input3(self, out):
+        out_put_list = out.strip().split('\n')
+        if len(out_put_list) == 0:
+            raise WrongAnswerException("The output can't be empty")
+        else:
+            first_line = out_put_list[0].strip().split(':')
+            if not first_line[1].strip() == NEW_SECOND_ANSWER:
+                raise WrongAnswerException("update function doesn't update answer properly")
+
+        return CheckResult.correct()
+
+    def test9_input3(self, out):
+        out_put_list = out.strip().split('\n')
+        if len(out_put_list) == 0:
+            raise WrongAnswerException("The output can't be empty")
+        else:
+            first_line = out_put_list[0].strip().split(':')
+            if len(first_line) < 2:
+                raise WrongAnswerException("The ':' is missing in output")
+            else:
+                out_put_list.pop(0)
+                check_learn_menu = '\n'.join(out_put_list)
+                if not first_line[0].strip() == 'Answer':
+                    raise WrongAnswerException('Answer is missing or misspelling')
+                if not first_line[1].strip() == NEW_SECOND_ANSWER:
+                    raise WrongAnswerException('before check_learn_menu the answer should be printed')
+                if not check_learn_menu.strip() == CHECK_LEARN_MENU.strip():
+                    raise WrongAnswerException(
+                        f'your check_learn_menu:\n{check_learn_menu} \ncorrect check_learn_ menu\n'
+                        f'{CHECK_LEARN_MENU} ')
+
+        return CheckResult.correct()
+
+    def test10_input11(self, out):
+        out_put = out.strip().split('\n')
+        if len(out_put) == 0:
+            raise WrongAnswerException("The output can't be empty")
+        else:
+            first_line = out_put[0].strip()
+            if not first_line == 'There is no flashcard to practice!':
+                raise WrongAnswerException('After three successive correct answers the question should be deleted from '
+                                           'database\nand the first line in the output should be like this:'
+                                           ' \nThere is no flashcard to practice!')
         return CheckResult.correct()
 
 
--- tool.py
+++ tool.py
@@ -1,6 +1,28 @@
 # write your code here
 # define variables and functions
-flashcard_dict = {}
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///flashcard.db?check_same_thread=False')
+
+Base = declarative_base()
+
+
+class Flashcard(Base):
+    __tablename__ = 'flashcard'
+
+    id = Column(Integer, primary_key=True)
+    question = Column(String)
+    answer = Column(String)
+    box = Column(Integer)
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 def print_menu():
@@ -36,26 +58,86 @@ def add_new_card():
                 if a == '':
                     continue
                 else:
-                    flashcard_dict[q] = a
+                    new_card = Flashcard(question=q, answer=a, box=1)
+                    session.add(new_card)
+                    session.commit()
                     return True
+
+
+def update(card):
+    print('press "d" to delete the flashcard:')
+    print('press "e" to edit the flashcard:')
+    a = input()
+    if a == 'd':
+        delete_card(card)
+    elif a == 'e':
+        print('current question:', card.question)
+        print('please write a new question:')
+        card.question = input()
+        if card.question != '':
+            print('current answer:', card.answer)
+            print('please write a new answer:')
+            new_answer = input()
+            if new_answer != '':
+                card.answer = new_answer
+                session.commit()
     else:
-        return False
+        print(a, 'is not an option')
+
+
+def move_card_to_next_session(card):
+    current_box = card.box
+    card.box = current_box + 1
+    session.commit()
+
+
+def move_card_to_session_one(card):
+    card.box == 1
+    session.commit()
+
+
+def delete_card(card):
+    session.delete(card)
+    session.commit()
+
+
+def print_learn_menu(card):
+    print('press "y" if your answer is correct:')
+    print('press "n" if your answer is wrong:')
+    a = input()
+    if a == 'y':
+        if card.box == 3:
+            delete_card(card)
+        else:
+            move_card_to_next_session(card)
+    elif a == 'n':
+        move_card_to_session_one(card)
+    else:
+        print(a, 'is not an option')
 
 
 def practice():
-    if len(flashcard_dict) == 0:
+    flashcard_list = session.query(Flashcard).all()
+    if len(flashcard_list) == 0:
         print('There is no flashcard to practice!')
     else:
-        for card in flashcard_dict:
-            print('Question: ', card)
-            print('Please press "y" to see the answer or press "n" to skip:')
+        for card in flashcard_list:
+            print('Question: ', card.question)
+            print('press "y" to see the answer:')
+            print('press "n" to skip:')
+            print('press "u" to update:')
+
             a = input()
             if a == 'y':
-                print('Answer: ', flashcard_dict[card])
-            else:
+                print('Answer: ', card.answer)
+                print_learn_menu(card)
+            elif a == 'n':
+                print_learn_menu(card)
                 continue
-
-# Start of program
+            elif a == 'u':
+                update(card)
+            else:
+                print(a, 'is not an option')
 
 
 while True:
